a13cc454ce9950e66e38ade9c3c168c283d96831
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-02-23 23:33:42 +0200

    add score

--- game.js
+++ game.js
@@ -1,15 +1,12 @@
 document.addEventListener('DOMContentLoaded', ()  => {
 
 var walls = document.getElementsByClassName('boundary');
-// for (let i = 0 ; i < lose.length-1 ; i++) {
-//     lose.add
-// }
-// walls.addEventListener('')
-// console.log(lose)
+
 var end_button = document.getElementById('end')
 var start_button = document.getElementById('start')
 var text = document.getElementById('status')
-
+var score = document.getElementsByClassName('example')[0]
+score_calculation = 0
 start_button.addEventListener('mouseover', start)
 
 function start(){
@@ -24,10 +21,15 @@ function youLost() {
     for (var i = 0 ; i < walls.length-1 ; i++){
         walls[i].classList.add('youlose')
     }
+    score_calculation -= 15
+    score.innerHTML = score_calculation
 }
 
 function youWin(){
     text.innerHTML = "You Won";
+    score_calculation +=5
+    score.innerHTML.style.text-align.center 
+    score.innerHTML = score_calculation
 }
 
 
