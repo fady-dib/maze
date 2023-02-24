4d30a1acafe3d8e1bb6d3c0c5ef50bb0637687ae
Fadi <fadi@Fadis-MacBook-Pro.local>
2023-02-24 02:13:56 +0200

    add removeEventListener

--- game.js
+++ game.js
@@ -1,51 +1,56 @@
 document.addEventListener('DOMContentLoaded', ()  => {
 
 var walls = document.getElementsByClassName('boundary');
-var end_button = document.getElementById('end')
-var start_button = document.getElementById('start')
-var text = document.getElementById('status')
-var score = document.getElementsByClassName('example')[0]
-var score_calculation = 0
-start_button.addEventListener('click', restart)
-start_button.addEventListener('mouseover', start)
-score.style.textAlign = "center"
+var end_button = document.getElementById('end');
+var start_button = document.getElementById('start');
+var text = document.getElementById('status');
+var score = document.getElementsByClassName('example')[0];
+var score_calculation = 0;
+start_button.addEventListener('click', restart);
+start_button.addEventListener('mouseover', start);
+score.style.textAlign = "center";
 
 function start(){
 for (var i = 0 ; i < walls.length-1 ; i++) {
-     walls[i].addEventListener('mouseover', youLost)
-     walls[i].classList.remove('youlose')
+     walls[i].addEventListener('mouseover', youLost);
+     walls[i].classList.remove('youlose');
 }
 
-end_button.addEventListener('mouseover', youWin)
+end_button.addEventListener('mouseover', youWin);
 
 }
 
 function youLost() {
-    text.innerHTML = "You Lost"
+    text.innerHTML = "You Lost";
     for (var i = 0 ; i < walls.length-1 ; i++){
-        walls[i].classList.add('youlose')
+        walls[i].classList.add('youlose');
+        walls[i].removeEventListener('mouseover',youLost);
     }
-    score_calculation -= 15
-    score.innerHTML = score_calculation
-    start_button.re
+    score_calculation -= 15;
+    score.innerHTML = score_calculation;
+    end_button.removeEventListener('mouseover', youWin);
     
 }
 
 function youWin(){
     text.innerHTML = "You Won";
-    score_calculation +=5
-    score.innerHTML = score_calculation
+    score_calculation +=5;
+    score.innerHTML = score_calculation;
+    end_button.removeEventListener('mouseover',youWin);
+    for (var i = 0 ; i < walls.length-1 ; i++){
+        walls[i].removeEventListener('mouseover',youLost);
+    }
 }
 
 function restart(){
     for (var i = 0 ; i < walls.length-1 ; i++){
-        walls[i].addEventListener('mouseover',youLost)
-        walls[i].classList.remove('youlose')
+        walls[i].addEventListener('mouseover',youLost);
+        walls[i].classList.remove('youlose');
     }
 
-    end_button.addEventListener('mouseover', youWin)
-    score_calculation = 0
-    score.innerHTML = score_calculation
+    end_button.addEventListener('mouseover', youWin);
+    score_calculation = 0;
+    score.innerHTML = score_calculation;
 
 }
 
